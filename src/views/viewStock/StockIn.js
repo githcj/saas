4c3a4e154633d8b693714afcf334bd4aaeaad1aa
meng <2562245299@qq.com>
2020-06-09 17:46:45 +0800

    6.9 meng 2

--- src/views/viewStock/StockIn.js
+++ src/views/viewStock/StockIn.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Table, DatePicker, Select } from 'antd'
+import { SyncOutlined, SearchOutlined, DownOutlined, UpOutlined, UnorderedListOutlined } from '@ant-design/icons'
+import '../../assets/css/viewStock/stockout.css'
 
 export default class StockIn extends Component{
     constructor(props){
@@ -7,8 +10,164 @@ export default class StockIn extends Component{
     render(){
         return (
             <div className="stockIn">
-                入库管理
+                <header>
+                    <div className="stock-top">
+                        <div className='stock-top-left'>
+                            <div className='stock-top-left-mark'></div>
+                            <p className='stock-top-word'>入库管理</p>
+                        </div>
+                        <div className='stock-top-right'>
+                            <SyncOutlined />
+                            <p className='stock-top-word'>刷新</p>
+                        </div>
+                    </div>
+                </header>
+
+                <section>
+                    <div className="screen">
+                        <div className="left">
+                            <SearchOutlined />
+                            <span>筛选查询</span>
+                        </div>
+                        <div className="right">
+                            <div>
+                                {/* <DownOutlined /> */}
+                                <UpOutlined />
+                                <span>收起筛选</span>
+                            </div>
+                            <div className="searchResult">查询结果</div>
+                        </div>
+                    </div>
+                    <div className="search">
+                        <div>
+                            入库日期：
+                            <DatePicker />
+                        </div>
+                        <div>
+                            供货厂商：
+                            <Select 
+                            defaultValue="供货厂商"
+                            style={{ width: 180 }}>
+                                <Option value="jack">Jack</Option>
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </div>
+                        <div>
+                            进货仓库：
+                            <Select 
+                            defaultValue="全部"
+                            style={{ width: 180 }}>
+                                <Option value="jack">Jack</Option>
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </div>
+                        <div>
+                            审核状态：
+                            <Select
+                                defaultValue="全部"
+                                style={{ width: 180 }}>
+                                <Option value="1">全部</Option>
+                                <Option value="2">待审批</Option>
+                                <Option value="3">已通过</Option>
+                            </Select>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="table">
+                    <div className="dataList">
+                        <div className="left">
+                            <UnorderedListOutlined />
+                            <span>数据列表</span>
+                        </div>
+                        <div className="right">
+                            <button type="button" className="add">添加</button>
+                            <button type="button">导出</button>
+                            <Select
+                                defaultValue="显示条数"
+                                style={{ width: 100 }}
+                                className="select">
+                                <Option value="1">1</Option>
+                                <Option value="2">2</Option>
+                                <Option value="3">3</Option>
+                            </Select>
+                            <Select
+                                defaultValue="排序方式"
+                                style={{ width: 100 }}
+                                className="select">
+                                <Option value="1">1</Option>
+                                <Option value="2">2</Option>
+                                <Option value="3">3</Option>
+                            </Select>
+                        </div>
+                    </div>
+                    <Table
+                        rowSelection={{ type: 'Checkbox' }}
+                        dataSource={data}
+                        columns={columns}
+                        bordered />
+                </div>
             </div>
         )
     }
 }
+const columns = [
+    {
+        title: '编号',
+        dataIndex: 'id',
+    },
+    {
+        title: '创建日期',
+        dataIndex: 'bulidDate',
+    },
+    {
+        title: '供货厂商',
+        dataIndex: 'store',
+    },
+    {
+        title:'进货仓库',
+        dataIndex:'salesMan'
+    },
+    {
+        title:'总金额',
+        dataIndex:'total'
+    },
+    {
+        title:'入库时间',
+        dataIndex:'deliveryTime'
+    },
+    {
+        title:'发起人',
+        dataIndex:'initiator'
+    },
+    {
+        title:'审批人',
+        dataIndex:'approver'
+    },
+    {
+        title:'状态',
+        dataIndex:'state'
+    },
+    {
+        title:'审批操作',
+        dataIndex:'approveState'
+    }
+];
+
+const data = [];
+for (let i = 0; i < 46; i++) {
+    data.push({
+        key: i,
+        id: i,
+        bulidDate: '2020-01-03',
+        store: 'A',
+        salesMan:'A',
+        total:'9999',
+        deliveryTime:'2020-06-06',
+        initiator:"A",
+        approver:'B',
+        state:'待审批',
+        approveState:'审批'
+    });
+}
+const { Option } = Select;
--- src/views/viewStock/StockOut.js
+++ src/views/viewStock/StockOut.js
@@ -1,76 +1,169 @@
 import React, { Component } from 'react'
-import { Table } from 'antd'
-import {SyncOutlined,SearchOutlined } from '@ant-design/icons'
+import { Table, DatePicker, Select } from 'antd'
+import { SyncOutlined, SearchOutlined, DownOutlined, UpOutlined, UnorderedListOutlined } from '@ant-design/icons'
 import '../../assets/css/viewStock/stockout.css'
 
 const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
+        title: '编号',
+        dataIndex: 'id',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
+        title: '创建日期',
+        dataIndex: 'bulidDate',
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
+        title: '出库仓库',
+        dataIndex: 'store',
     },
-  ];
-  
-  const data = [];
-  for (let i = 0; i < 46; i++) {
-    data.push({
-      key: i,
-      name: `Edward King ${i}`,
-      age: 32,
-      address: `London, Park Lane no. ${i}`,
-    });
-  }
-
-
-export default class StockOut extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            search:[{
-
-            }]
-        }
+    {
+        title:'业务员',
+        dataIndex:'salesMan'
+    },
+    {
+        title:'总金额',
+        dataIndex:'total'
+    },
+    {
+        title:'出库时间',
+        dataIndex:'deliveryTime'
+    },
+    {
+        title:'发起人',
+        dataIndex:'initiator'
+    },
+    {
+        title:'审批人',
+        dataIndex:'approver'
+    },
+    {
+        title:'状态',
+        dataIndex:'state'
+    },
+    {
+        title:'审批操作',
+        dataIndex:'approveState'
     }
-    render(){
+];
+
+const data = [];
+for (let i = 0; i < 46; i++) {
+    data.push({
+        key: i,
+        id: i,
+        bulidDate: '2020-01-03',
+        store: 'A',
+        salesMan:'A',
+        total:'9999',
+        deliveryTime:'2020-06-06',
+        initiator:"A",
+        approver:'B',
+        state:'待审批',
+        approveState:'审批'
+    });
+}
+const { Option } = Select;
+
+
+export default class StockOut extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
         return (
             <div className="stockOut">
                 <header>
-                    <div className="stockout-top">
-                        <div className='stockout-top-left'>
-                            <div className='stockout-top-leftmark'></div>
-                            <p className='stockout-top-word'>出库管理</p>
+                    <div className="stock-top">
+                        <div className='stock-top-left'>
+                            <div className='stock-top-left-mark'></div>
+                            <p className='stock-top-word'>出库管理</p>
                         </div>
-                        <div className='stockout-top-right'>
+                        <div className='stock-top-right'>
                             <SyncOutlined />
-                            <p className='stockout-top-word'>刷新</p>
+                            <p className='stock-top-word'>刷新</p>
                         </div>
                     </div>
                 </header>
 
                 <section>
                     <div className="screen">
-                        <SearchOutlined />
-                        <span>筛选查询</span>
+                        <div className="left">
+                            <SearchOutlined />
+                            <span>筛选查询</span>
+                        </div>
+                        <div className="right">
+                            <div>
+                                {/* <DownOutlined /> */}
+                                <UpOutlined />
+                                <span>收起筛选</span>
+                            </div>
+                            <div className="searchResult">查询结果</div>
+                        </div>
                     </div>
                     <div className="search">
-
+                        <div>
+                            出库日期：
+                            <DatePicker />
+                        </div>
+                        <div>
+                            出库仓库：
+                            <Select style={{ width: 180 }}>
+                                <Option value="jack">Jack</Option>
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </div>
+                        <div>
+                            客户名称：
+                            <Select style={{ width: 180 }}>
+                                <Option value="jack">Jack</Option>
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </div>
+                        <div>
+                            审核状态：
+                            <Select
+                                defaultValue="全部"
+                                style={{ width: 180 }}>
+                                <Option value="1">全部</Option>
+                                <Option value="2">待审批</Option>
+                                <Option value="3">已通过</Option>
+                            </Select>
+                        </div>
                     </div>
                 </section>
 
                 <div className="table">
-                    <Table 
-                    dataSource={data} 
-                    columns={columns} 
-                    style={{
-                        border:'1px solid'
-                    }}/>
+                    <div className="dataList">
+                        <div className="left">
+                            <UnorderedListOutlined />
+                            <span>数据列表</span>
+                        </div>
+                        <div className="right">
+                            <button type="button" className="add">添加</button>
+                            <button type="button">导出</button>
+                            <Select
+                                defaultValue="显示条数"
+                                style={{ width: 100 }}
+                                className="select">
+                                <Option value="1">1</Option>
+                                <Option value="2">2</Option>
+                                <Option value="3">3</Option>
+                            </Select>
+                            <Select
+                                defaultValue="排序方式"
+                                style={{ width: 100 }}
+                                className="select">
+                                <Option value="1">1</Option>
+                                <Option value="2">2</Option>
+                                <Option value="3">3</Option>
+                            </Select>
+                        </div>
+                    </div>
+                    <Table
+                        rowSelection={{ type: 'Checkbox' }}
+                        dataSource={data}
+                        columns={columns}
+                        bordered />
                 </div>
             </div>
         )
