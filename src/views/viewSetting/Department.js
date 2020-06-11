171ca57738e6e24f803d98fad217d8ecc989d7f4
MR.mr <1390609881@qq.com>
2020-06-11 09:29:24 +0800

    '公司信息1.0

--- src/views/viewSetting/Department.js
+++ src/views/viewSetting/Department.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react'
-import { Table } from 'antd'
-import {SyncOutlined,SearchOutlined } from '@ant-design/icons'
-import '../../assets/css/viewStock/stockout.css'
+import { Table, Select } from 'antd'
+import ConTitle from '../../components/ConTitle'
+import { UnorderedListOutlined } from '@ant-design/icons'
+import '../../assets/css/viewSetting/Department.css'
+
+const { Option } = Select
 
 const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
+      title: '部门名称',
+      dataIndex: 'dep_name',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
+      title: '职能描述',
+      dataIndex: 'dep_describe',
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-    },
+      title: '员工数量',
+      dataIndex: 'emp_num',
+    },{
+        title: '添加时间',
+        dataIndex: 'dep_addtime'
+    },{
+        title: '是否启用',
+        
+    },{
+        title: '操作'
+    }
   ];
   
   const data = [];
   for (let i = 0; i < 46; i++) {
     data.push({
       key: i,
-      name: `Edward King ${i}`,
-      age: 32,
-      address: `London, Park Lane no. ${i}`,
+      dep_name: `Edward King ${i}`,
+      dep_describe: '无描述',
+      emp_num: `${i}`,
+      dep_addtime:'2020-12-6'
     });
   }
 
+  
 
 export default class Department extends Component{
     constructor(props){
@@ -38,30 +51,43 @@ export default class Department extends Component{
             }]
         }
     }
+
+    handleChange = () =>{
+
+    }
     render(){
         return (
             <div className="stockOut">
                 <header>
                     <div className="stockout-top">
-                        <div className='stockout-top-left'>
-                            <div className='stockout-top-leftmark'></div>
-                            <p className='stockout-top-word'>出库管理</p>
-                        </div>
-                        <div className='stockout-top-right'>
-                            <SyncOutlined />
-                            <p className='stockout-top-word'>刷新</p>
-                        </div>
+                        <ConTitle titleName='部门管理' />
                     </div>
                 </header>
-
+                <div className='dynamic-dataList'>
+                    <div className='dynamic-left-title'>
+                        <UnorderedListOutlined style={{fontSize:'20px'}}/>
+                        <span style={{fontSize:'14px'}}>数据列表</span>
+                    </div>
+                    <div className="purchase-table-se2">
+                        <div className="addDiv">添加</div>
+                        <Select defaultValue="显示条数" style={{ width: 120 }} onChange={this.handleChange}>
+                            <Option value="15">10条/页</Option>
+                            <Option value="20">15条/页</Option>
+                        </Select>
+                        <Select defaultValue="排序方式" className="seen" style={{ width: 120 }} onChange={this.handleChange}>
+                            <Option value="15">编号递增</Option>
+                            <Option value="20">编号递减</Option>
+                        </Select>
+                    </div>
+                </div>
 
                 <div className="table">
                     <Table 
                     dataSource={data} 
                     columns={columns} 
-                    style={{
-                        border:'1px solid'
-                    }}/>
+                    bordered
+
+                    />
                 </div>
             </div>
         )
--- src/views/viewSetting/Gongsi.js
+++ src/views/viewSetting/Gongsi.js
@@ -1,21 +1,187 @@
 import React from 'react'
-// import { Button, NavBar, Icon } from 'antd-mobile'
+import { Form, Input, Tooltip, Button, InputNumber } from 'antd';
+import ConTitle from '../../components/ConTitle'
+import WXUpLoad from '../../components/WXUpLoad'
+import CompanyImgs from '../../components/CompanyImgs'
 import {
-  SyncOutlined,
+    EnvironmentOutlined
 } from '@ant-design/icons';
+import '../../assets/css/viewSetting/Gongsi.css'
 
 const Gongsi = (props)=> {
-  // const {match, history} = props
-  return (
-    <div className="gongsi">
-      <div className='gongsi-firtop'>
-                <div className='gongsi-top-left'>
-                    <div className='gongsi-top-left-mark'></div>
-                    <p className='gongsi-top-word'>公司信息</p>
-                </div>
+    const { TextArea } = Input;
+    
+    const layout = {
+        labelCol: {
+          span: 8,
+        },
+        wrapperCol: {
+          span: 8,
+        },
+    };
+
+      
+    const Demo = () => {
+        const onFinish = values => {
+            console.log(values);
+        };
+    }
+
+    const onFinish = () => {
+
+    }
+        
+      
+    return (
+        <div className="gongsi">
+            <div className='gongsi-firtop'>
+                <ConTitle titleName='????'/>
             </div>
-    </div>
-  )
+            <div className='company-content'>
+                <Form {...layout} name="nest-messages" onFinish={onFinish} size="large">
+                    <Form.Item
+                        name={['company', 'name']}
+                        label="????"
+                        rules={[
+                        {
+                            required: true,
+                            message: '???????!' 
+                        },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'address']}
+                        label="????"
+                        rules={[
+                        {
+                            required: true,
+                            message: '???????!' 
+                        },
+                        ]}
+                    >
+                        <Input
+                            suffix={
+                                <Tooltip title="????">
+                                    <EnvironmentOutlined />
+                                </Tooltip>
+                            }
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'staffNum']}
+                        label="???"
+                        rules={[
+                        {
+                            required: true,
+                            message: '??????!' 
+                        },
+                        ]}
+                    >
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'charge']}
+                        label="???"
+                        rules={[
+                        {
+                            required: true,
+                            message: '???????!' 
+                        },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'IDCard']}
+                        label="????"
+                        rules={[
+                        {
+                            required: true,
+                            message:'???????'
+                        },{ 
+                            pattern:/^[1-9]\d{5}(18|19|20|(3\d))\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/,
+                            message:'???????????'
+                         }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'phoneNum']}
+                        label="????"
+                        rules={[
+                        {
+                            required: true,
+                            message:'???????'
+                        },{ pattern:/^1[3456789]\d{9}$/ ,
+                            message:'???????????'
+                        }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'email']}
+                        label="????"
+                        rules={[
+                        {
+                            required: true,
+                            message:'???????'
+                        },{ 
+                            type:"email",
+                            message:'????????'
+                        }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'wx']}
+                        label="?????"
+                        rules={[
+                        {
+                            required: true,
+                            message:'??????????'
+                        }
+                        ]}
+                    >
+                        <WXUpLoad notify='???????' />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'logo']}
+                        label="??logo"
+                        rules={[
+                        {
+                            required: true,
+                            message:'???logo??'
+                        }
+                        ]}
+                    >
+                        <WXUpLoad notify='??logo??' />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'instr']}
+                        label="??"
+                    >
+                        <TextArea rows={4} />
+                    </Form.Item>
+                    <Form.Item
+                        name={['company', 'companyImgs']}
+                        label="?????"
+                    >
+                        <CompanyImgs />
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                        <Button type="primary" htmlType="submit">
+                        ??
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
 }
 
 export default Gongsi
