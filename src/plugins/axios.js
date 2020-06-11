e70f918a3e03f938d0d6fdac758a34cd310a831b
MR.mr <1390609881@qq.com>
2020-06-11 10:25:16 +0800

    'axios封装1.0.0'

---
+++ src/plugins/axios.js
@@ -0,0 +1,86 @@
+"use strict";
+import axios from "axios";
+import {Router} from  'react-router-dom'
+
+
+let config = {
+    baseURL: "http://119.23.228.238:3031/mock/47",
+    timeout: 60*1000, // Timeout
+};
+
+const _axios = axios.create(config);
+
+// 设置post请求头
+_axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
+_axios.interceptors.request.use(
+    function(config) {
+    // Do something before request is sent
+        const token = window.localStorage.getItem('token');
+        if (token) {
+          // 设置 token ，一般是在 headers 里加入 Authorization，并加上 Bearer 标注
+          // 最好通过此种形式设置 request.headers['Authorization']
+          config.headers['Authorization'] = token; // 基于 nodejs
+        //   权限设置也需要在请求头里携带信息
+        }
+        return config;
+    },
+    function(error) {
+    // Do something with request error
+    
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+_axios.interceptors.response.use(
+    function(response) {
+    // Do something with response data
+        //console.log(response,'HHHHH')
+        // loadingInstance.close() // 关闭 loading
+
+        if (response.data.token) { // 将返回的最新的 token 保存
+          window.localStorage.setItem('token', response.data.token);
+        }
+          
+        return response;
+    },
+    function(error) {
+		console.log(error.response.status,'??')
+        if (error.response.status === 401) {
+            // 401 说明 token 验证失败
+            // 可以直接跳转到登录页面，重新登录获取 token
+            window.localStorage.removeItem('token');
+            console.log(error.response.data.error.message,'token过期');
+            window.history.go('http://localhost:3000/login')
+        }else if(error.response.status === 500) {
+            // 服务器错误
+            alert('服务器错误,请稍后再试')
+            return Promise.reject('服务器出错：',error.response.data);
+        }
+        // Do something with response error
+        return Promise.reject(error);
+    }
+);
+
+Plugin.install = function(Vue, options) {
+    console.log(options);
+    Vue.axios = _axios;
+    window.axios = _axios;
+    Object.defineProperties(Vue.prototype, {
+        axios: {
+            get() {
+                return _axios;
+            }
+        },
+        $axios: {
+            get() {
+                return _axios;
+            }
+        },
+    });
+};
+
+Vue.use(Plugin)
+
+export default Plugin;
