07aa88d4064caf3a5ea4a9fa9ca0ce8c56d00103
meng <2562245299@qq.com>
2020-06-16 20:16:03 +0800

    6.16 meng 2

--- src/views/viewIndex/Caigou.js
+++ src/views/viewIndex/Caigou.js
@@ -16,7 +16,7 @@ const Caigou = (props) => {
 						<Menu style={{ background: '#f3f3f3' }}>
 							<Menu.ItemGroup key="g1" title="基本资料">
 								<Menu.Item key="1">
-									<NavLink to={match.url + "/purchaseAdmin"}>采购管理</NavLink>
+									<NavLink to={match.url}>采购管理</NavLink>
 								</Menu.Item>
 								<Menu.Item key="2">
 									<NavLink to={match.url + "/purchaseApply"}>采购申请</NavLink>
@@ -28,14 +28,14 @@ const Caigou = (props) => {
 				</Col>
 				<Col span={20}>
 					<div className="show">
-						<Route path={match.url + "/purchaseAdmin"}>
+						<Route path={match.url} exact>
 							<PurchaseAdmin msg={history} />
 						</Route>
 						<Route path={match.url + "/purchaseApply"} component={purchaseApply}>
 						</Route>
 						<Route path={match.url + "/purchaseDetail"} component={purchaseDetail}>
 						</Route>
-						<Redirect exact to={match.url + '/purchaseAdmin'}></Redirect>
+						{/* <Redirect exact to={match.url + '/purchaseAdmin'}></Redirect> */}
 					</div>
 				</Col>
 			</Row>
